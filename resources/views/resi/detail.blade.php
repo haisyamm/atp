971af35fe13758535bf67acd205158ac5d6923c0
kanglerian <kanglerian@gmail.com>
2022-09-28 16:07:17 +0700

    add change format number

--- resources/views/resi/detail.blade.php
+++ resources/views/resi/detail.blade.php
@@ -177,11 +177,11 @@
                                     </div>
                                     <div class="col-md-6 form-group mb-3">
                                         <label for="tlp_pengirim" class="small fw-bolder text-uppercase">Telp. Pengirim</label>
-                                        <p class="mt-2">{{ $resi->tlp_pengirim}}</p>                                                    
+                                        <p class="mt-2">{{ changeNumber($resi->tlp_pengirim) }}</p>                                                    
                                     </div>                        
                                     <div class="col-md-6 form-group mb-3">
                                         <label for="tlp_penerima" class="small fw-bolder text-uppercase">Telp. Penerima</label>
-                                        <p class="mt-2">{{ $resi->tlp_penerima}}</p>
+                                        <p class="mt-2">{{ changeNumber($resi->tlp_penerima) }}</p>
                                     </div>
                                 </div>
                             </div>
@@ -338,6 +338,29 @@
 </div>
 @endsection
 @push('script')
-<script>
-</script>
+<?php
+ function changeNumber($nohp){
+     // kadang ada penulisan no hp 0811 239 345
+     $nohp = str_replace(" ","",$nohp);
+     // kadang ada penulisan no hp (0274) 778787
+     $nohp = str_replace("(","",$nohp);
+     // kadang ada penulisan no hp (0274) 778787
+     $nohp = str_replace(")","",$nohp);
+     // kadang ada penulisan no hp 0811.239.345
+     $nohp = str_replace(".","",$nohp);
+ 
+     // cek apakah no hp mengandung karakter + dan 0-9
+     if(!preg_match('/[^+0-9]/',trim($nohp))){
+         // cek apakah no hp karakter 1-3 adalah +62
+         if(substr(trim($nohp), 0, 3)=='+62'){
+             $hp = trim($nohp);
+         }
+         // cek apakah no hp karakter 1 adalah 0
+         elseif(substr(trim($nohp), 0, 1)=='0'){
+             $hp = '+62'.substr(trim($nohp), 1);
+         }
+     }
+     print $hp;
+ }
+?>
 @endpush
