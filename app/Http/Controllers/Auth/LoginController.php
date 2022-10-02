235e546dd8a56367dd1e4fe269ad377dd4e2c0e8
haisyamm <haisyammaulana22@gmail.com>
2022-10-02 20:47:18 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -53,7 +53,7 @@ class LoginController extends Controller
             if (auth()->user()->type == 1) {
                 return redirect()->route('lm-admin');
             }else if (auth()->user()->type == 2) {
-                return redirect()->route('lm-admine');
+                return redirect()->route('lm-admin');
             }else if (auth()->user()->type == 0) {
                 return redirect()->route('lm-admin');
             }else{
